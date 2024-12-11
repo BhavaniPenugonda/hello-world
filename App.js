a61b6433a6049aceb428a73894560d0b98969314
BhavaniPenugonda <bhavani0202@gmail.com>
2024-12-11 14:57:31 +0100

    reset is done

--- App.js
+++ App.js
@@ -3,31 +3,13 @@ import { StyleSheet,  View } from 'react-native';
 
 const App=()=> {
   return (
-    <View style={styles.container}>
-      <View style={styles.box1}></View>
-      <View style={styles.box2}></View>
-      <View style={styles.box3}></View>
+    <View >
     </View>
     
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    
-  },
-  box1: {
-    flex:1,
-    backgroundColor: 'blue'
-  },
-  box2: {
-    flex:12,
-    backgroundColor: 'red'
-  },
-  box3: {
-    flex:5,
-    backgroundColor: 'green'
-  }
+  
 });
 export default App;
