4796783143ea49416cf6920576bb3ed80dc1636e
BhavaniPenugonda <bhavani0202@gmail.com>
2024-12-11 15:09:06 +0100

    TextInput component is created ,imported and styles are applied

--- App.js
+++ App.js
@@ -1,17 +1,37 @@
 
-import { StyleSheet, TextInput, View } from 'react-native';
+import { StyleSheet, TextInput, View ,Text} from 'react-native';
 import { useState } from 'react';
 
 const [text, setText] = useState('');
 const App=()=> {
   return (
-    <View >
+    <View ><TextInput 
+    style={styles.textInput}
+    value={text}
+    onChangeText={setText}
+    placeholder='React-Native'
+  />
+  <Text style={styles.textDisplay}>You wrote: {text}</Text>
     </View>
     
   );
 }
 
 const styles = StyleSheet.create({
-  
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  textInput: {
+    width: '88%',
+    borderWidth: 1,
+    height: 50,
+    padding: 10
+  },
+  textDisplay: {
+    height: 50,
+    lineHeight: 50
+  }
 });
 export default App;
