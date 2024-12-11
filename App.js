10a9f22bb69b373a3f0dfa911a06faa67722645d
BhavaniPenugonda <bhavani0202@gmail.com>
2024-12-11 15:00:09 +0100

    New state is created. And also TextInput is imported

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 
-import { StyleSheet,  View } from 'react-native';
+import { StyleSheet, TextInput, View } from 'react-native';
+import { useState } from 'react';
 
+const [text, setText] = useState('');
 const App=()=> {
   return (
     <View >
