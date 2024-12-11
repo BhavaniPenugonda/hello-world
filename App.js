af63b965334bfac95cad42200a8dcdd405357a7d
BhavaniPenugonda <bhavani0202@gmail.com>
2024-12-11 14:36:06 +0100

    reset is done

--- App.js
+++ App.js
@@ -1,37 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { StyleSheet,  View } from 'react-native';
 
 const App=()=> {
   return (
-    <View style={styles.container}>
-      <Text style={styles.blue}>Hello World!</Text>
-      <Text style={styles.bigRed}>How are you?</Text>
-      <Text style= {[styles.blue, styles.bigRed]}>I'm feeling blue!</Text>
-      <View style={styles.box}></View>
+    <View >
     </View>
     
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  blue: {
-    color: 'blue',
-    fontWeight: '600',
-  },
-  bigRed: {
-    color: 'red',
-    fontSize: 30,
-  },
-  box: {
-    width: 60,
-    height: 60,
-    backgroundColor: 'blue',
-  }
+  
 });
 export default App;
